39448eee2c5518868a417f82cb88927da0b1f82a
juandent <juandent@mac.com>
2016-08-19 14:00:30 -0600

    Initial Commit

---
+++ TemplateMetaprogramming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TemplateMetaprogramming
+//
+//  Created by Juan Dent on 19/August/16.
+//  Copyright © 2016 Juan Dent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
