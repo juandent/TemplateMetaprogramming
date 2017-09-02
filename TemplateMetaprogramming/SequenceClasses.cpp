13420f355b6edaf457e7a4e6c92d2590539791e3
JUAN DENT <juandent@mac.com>
2017-09-02 17:18:51 -0600

    working at testTiny()

--- TemplateMetaprogramming/SequenceClasses.cpp
+++ TemplateMetaprogramming/SequenceClasses.cpp
@@ -57,7 +57,8 @@ namespace SequenceClasses {
 			typedef mpl::push_back<s10, int[11]>::type s11;
 
 		}
-} }
+	}
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -66,140 +67,143 @@ namespace SequenceClasses {
 // summary:	Section 5.11 pg 97; C++ Template Metaprogramming.
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	struct none {};
+struct none {};
 
 #ifdef NO_TAG
-		template<typename T0 = none, typename T1 = none, typename T2 = none>
-		struct tiny
-		{
-			typedef tiny type;
-			typedef T0 t0;
-			typedef T1 t1;
-			typedef T2 t2;
-		};
+template<typename T0 = none, typename T1 = none, typename T2 = none>
+struct tiny
+{
+	typedef tiny type;
+	typedef T0 t0;
+	typedef T1 t1;
+	typedef T2 t2;
+};
 	}
 
 #else
-	struct tiny_tag {};
+struct tiny_tag {};
 
-	template<typename T0 = none, typename T1 = none, typename T2 = none>
-	struct tiny
-	{
-		typedef tiny_tag	tag;
-		typedef tiny		type;
-		typedef T0			t0;
-		typedef T1			t1;
-		typedef T2			t2;
-	};
+template<typename T0 = none, typename T1 = none, typename T2 = none>
+struct tiny
+{
+	typedef tiny_tag	tag;
+	typedef tiny		type;
+	typedef T0			t0;
+	typedef T1			t1;
+	typedef T2			t2;
+};
 #endif
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/// <summary>	Manipulate a tiny. </summary>
-	///
-	/// <remarks>	Juan Dent, 21/3/2017. Very simple due to tiny's straightforward definition</remarks>
-	///
-	/// <typeparam name="Tiny">	Type of the tiny. </typeparam>
-	////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// <summary>	Manipulate a tiny. </summary>
+///
+/// <remarks>	Juan Dent, 21/3/2017. Very simple due to tiny's straightforward definition</remarks>
+///
+/// <typeparam name="Tiny">	Type of the tiny. </typeparam>
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	template<typename Tiny>
-	struct manipulate_tiny
-	{
-		typedef typename Tiny::t0 t0;
-		// ...
-	};
+template<typename Tiny>
+struct manipulate_tiny
+{
+	typedef typename Tiny::t0 t0;
+	// ...
+};
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/// <summary>	A tiny iterator. </summary>
-	///
-	/// <remarks>	Juan Dent, 21/3/2017.
-	/// 			We do not place next and prev as part of the iterator in order to avoid the
-	/// 			blob anti-pattern </remarks>
-	///
-	/// <typeparam name="Tiny">	Type of the tiny. </typeparam>
-	/// <typeparam name="Pos"> 	Type of the position (integral wrapper). </typeparam>
-	////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// <summary>	A tiny iterator. </summary>
+///
+/// <remarks>	Juan Dent, 21/3/2017.
+/// 			We do not place next and prev as part of the iterator in order to avoid the
+/// 			blob anti-pattern </remarks>
+///
+/// <typeparam name="Tiny">	Type of the tiny. </typeparam>
+/// <typeparam name="Pos"> 	Type of the position (integral wrapper). </typeparam>
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	template<typename Tiny, typename Pos>
-	struct tiny_iterator
-	{
-		typedef mpl::random_access_iterator_tag category;
-		typedef tiny_iterator	type;
+template<typename Tiny, typename Pos>
+struct tiny_iterator
+{
+	typedef mpl::random_access_iterator_tag category;
+	typedef tiny_iterator	type;
 
-		// More OO design - yet it fails to make each metafunction serve one goal well and only one
-		// not good idea:
-		// TODO: test these!!
-		// Actually, the state of this struct is NOT changed by either of the following metafunctions!
-		// So, there is no compelling reason from OO philosophy to nest them here complicating this 
-		// struct unnecessarily...
-		typedef typename mpl::next<Pos>::type next;
-		typedef typename mpl::prior<Pos>::type prior;
+	static_assert(Pos::value < 3 && Pos::value >= 0);
 
-	};
+	// More OO design - yet it fails to make each metafunction serve one goal well and only one
+	// not good idea:
+	// TODO: test these!!
+	// Actually, the state of this struct is NOT changed by either of the following metafunctions!
+	// So, there is no compelling reason from OO philosophy to nest them here complicating this 
+	// struct unnecessarily...
+	typedef typename mpl::next<Pos>::type next;
+	typedef typename mpl::prior<Pos>::type prior;
 
-	template<typename Tiny, typename Pos>
-	struct mpl::next<tiny_iterator<Tiny, Pos>>
-	{
-		typedef tiny_iterator <
-			Tiny,
-			typename mpl::next<Pos>::type
-		> type;
-	};
+};
 
-	template<typename Tiny, typename Pos>
-	struct mpl::prior<tiny_iterator<Tiny, Pos>>
-	{
-		typedef tiny_iterator <
-			Tiny,
-			typename mpl::prior<Pos>::type
-		> type;
-	};
+template<typename Tiny, typename Pos>
+struct mpl::next<tiny_iterator<Tiny, Pos>>
+{
+	typedef tiny_iterator <
+		Tiny,
+		typename mpl::next<Pos>::type
+	> type;
+};
 
-	template<typename Tiny, size_t N>
-	struct tiny_at;
+template<typename Tiny, typename Pos>
+struct mpl::prior<tiny_iterator<Tiny, Pos>>
+{
+	typedef tiny_iterator <
+		Tiny,
+		typename mpl::prior<Pos>::type
+	> type;
+};
 
-	// partially specialized accessors for each index
-	template<typename Tiny>
-	struct tiny_at<Tiny, 0>
-	{
-		typedef typename Tiny::t0	type;
-	};
+template<typename Tiny, size_t N>
+struct tiny_at;
 
-	template<typename Tiny>
-	struct tiny_at<Tiny, 1>
-	{
-		typedef typename Tiny::t1	type;
-	};
+// partially specialized accessors for each index
+template<typename Tiny>
+struct tiny_at<Tiny, 0>
+{
+	typedef typename Tiny::t0	type;
+};
 
-	template<typename Tiny>
-	struct tiny_at<Tiny, 2>
-	{
-		typedef typename Tiny::t2	type;
-	};
+template<typename Tiny>
+struct tiny_at<Tiny, 1>
+{
+	typedef typename Tiny::t1	type;
+};
 
-	// partialy specialize mpl::at for tiny sequences:
+template<typename Tiny>
+struct tiny_at<Tiny, 2>
+{
+	typedef typename Tiny::t2	type;
+};
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
-	/// <summary>	An at. </summary>
-	///
-	/// <remarks>	Juan Dent, 21/3/2017. </remarks>
-	///
-	/// <typeparam name="T0"> 	Generic type parameter. </typeparam>
-	/// <typeparam name="T1"> 	Generic type parameter. </typeparam>
-	/// <typeparam name="T2"> 	Generic type parameter. </typeparam>
-	/// <typeparam name="Pos">	Type of the position (Integral wrapper). </typeparam>
-	////////////////////////////////////////////////////////////////////////////////////////////////////
+// partialy specialize mpl::at for tiny sequences:
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// <summary>	An at. </summary>
+///
+/// <remarks>	Juan Dent, 21/3/2017. </remarks>
+///
+/// <typeparam name="T0"> 	Generic type parameter. </typeparam>
+/// <typeparam name="T1"> 	Generic type parameter. </typeparam>
+/// <typeparam name="T2"> 	Generic type parameter. </typeparam>
+/// <typeparam name="Pos">	Type of the position (Integral wrapper). </typeparam>
+////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifdef SPECIALIZING_AT
 
-	template<typename T0, typename T1, typename T2, typename Pos>
-	struct mpl::at<tiny<T0, T1, T2>, Pos> 
-		: tiny_at<tiny<T0, T1, T2>, Pos::value>
-	{
-	};
+template<typename T0, typename T1, typename T2, typename Pos>
+struct mpl::at<tiny<T0, T1, T2>, Pos>
+	: tiny_at<tiny<T0, T1, T2>, Pos::value>
+{
+};
 #endif
 
 #ifdef DEFINE_INSIDE_NAMESPACES_EXPLICITLY
 
-namespace boost { namespace mpl {
+namespace boost {
+	namespace mpl {
 
 #ifndef NO_TAG
 		template<>
@@ -218,7 +222,7 @@ namespace boost { namespace mpl {
 		{};
 #endif
 #endif
-		
+
 		// now deref iterator
 		template<typename Tiny, typename Pos>
 		struct deref< tiny_iterator<Tiny, Pos>> : at<Tiny, Pos>
@@ -232,8 +236,34 @@ namespace boost { namespace mpl {
 #ifdef DEFINE_WITH_QUALIFIED_NAMESPACE_BUT_AT_GLOBAL_SCOPE
 
 #ifndef NO_TAG
+template<>
+struct mpl::at_impl<tiny_tag>
+{
+	template<typename Tiny, typename N>
+	struct apply : tiny_at<Tiny, N::value>
+	{};
+};
+
+#if 1
+template<typename Sequence, typename N>
+struct at
+	: mpl::at_impl<typename Sequence::tag>
+	::template apply<Sequence, N>
+{};
+#endif
+#endif
+
+// now deref iterator
+template<typename Tiny, typename Pos>
+struct mpl::deref< tiny_iterator<Tiny, Pos>> : at<Tiny, Pos>
+{};
+#else		// DEFINE_WITH_QUALIFIED_NAMESPACE_BUT_AT_ANOTHER_NAMESPACE_SCOPE
+
+namespace Foo
+{
+#ifndef NO_TAG
 	template<>
-	struct mpl::at_impl<tiny_tag>
+	struct mpl::at_impl<tiny_tag>				// specialization not allowed in this namespace Foo
 	{
 		template<typename Tiny, typename N>
 		struct apply : tiny_at<Tiny, N::value>
@@ -251,36 +281,10 @@ namespace boost { namespace mpl {
 
 	// now deref iterator
 	template<typename Tiny, typename Pos>
-	struct mpl::deref< tiny_iterator<Tiny, Pos>> : at<Tiny, Pos>
+	struct ::mpl::deref< tiny_iterator<Tiny, Pos>> : at<Tiny, Pos>
 	{};
-#else		// DEFINE_WITH_QUALIFIED_NAMESPACE_BUT_AT_ANOTHER_NAMESPACE_SCOPE
 
-	namespace Foo
-	{
-#ifndef NO_TAG
-		template<>
-		struct mpl::at_impl<tiny_tag>				// specialization not allowed in this namespace Foo
-		{
-			template<typename Tiny, typename N>
-			struct apply : tiny_at<Tiny, N::value>
-			{};
-		};
-
-#if 0
-		template<typename Sequence, typename N>
-		struct at
-			: at_impl<typename Sequence::tag>
-			::template apply<Sequence, N>
-		{};
-#endif
-#endif
-
-		// now deref iterator
-		template<typename Tiny, typename Pos>
-		struct ::mpl::deref< tiny_iterator<Tiny, Pos>> : at<Tiny, Pos>
-		{};
-
-	}
+}
 #endif
 #endif
 
@@ -299,15 +303,29 @@ namespace boost { namespace mpl {
 template<typename Tiny, typename Pos, typename N>
 struct mpl::advance< tiny_iterator<Tiny, Pos>, N>
 {
-	typedef tiny_iterator<
+	typedef typename tiny_iterator<
 		Tiny,
 		typename mpl::plus<Pos, N>::type		// metafunction for adding integral wrappers
-	>	type;
+	>::type	type;
+	// this should be in tiny_iterator:
+	static_assert(Pos::value < 4 && Pos::value >= 0);
+
+	//typedef typename mpl::advance<tiny_iterator<Tiny, mpl::int_<5>>, mpl::int_<2>>::type seven;
+	//typedef typename tiny_iterator<Tiny, mpl::int_<5>>::type fifth;
+
+	// test position of final iterator
+private:
+	static constexpr int final_pos = mpl::plus<Pos, N>::value;
+	static_assert(final_pos < 4 && final_pos >= 0);
+
 };
 
 template<typename Tiny, typename Pos1, typename Pos2>
 struct mpl::distance < tiny_iterator<Tiny, Pos1>, tiny_iterator<Tiny, Pos2>> : mpl::minus<Pos2, Pos1>
-{};
+{
+private:
+
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// <summary>	A begin implementation - returns iterator to first element of sequence </summary>
@@ -343,8 +361,8 @@ struct mpl::end_impl<tiny_tag>
 {
 	// count how many none elements
 	template<typename Tiny>
-	struct apply  :  
-		mpl::eval_if<is_same<none, typename Tiny::t0>, 
+	struct apply :
+		mpl::eval_if<is_same<none, typename Tiny::t0>,
 		tiny_iterator<Tiny, mpl::int_<0>>,
 		mpl::eval_if<is_same<none, typename Tiny::t1>,
 		tiny_iterator<Tiny, mpl::int_<1>>,
@@ -366,7 +384,7 @@ struct tiny_size;
 #ifdef INHERITING_TYPE_FROM_INT_
 
 template<>
-struct tiny_size<none, none, none> 
+struct tiny_size<none, none, none>
 	: mpl::int_<0>
 {};
 
@@ -381,16 +399,23 @@ struct tiny_size<T0, T1, none>
 {};
 
 template<typename T0, typename T1, typename T2>
-struct tiny_size 
+struct tiny_size
 	: mpl::int_<3>
 {};
 
-//create an "overload" of tiny_size!! : NOT POSSIBLE
 
 template<typename Tiny>
-struct tiny_size_composite 
-	: tiny_size< typename Tiny::t0, typename Tiny::t1, typename Tiny::t2> 
+struct tiny_size_composite
+	: tiny_size< typename Tiny::t0, typename Tiny::t1, typename Tiny::t2>
 {};
+
+
+void tryComposite()
+{
+	typedef tiny<int, int, int> three_ints;
+	auto size = tiny_size_composite<three_ints>::value;
+
+}
 
 #else	// SPECIFYING type inside struct
 
@@ -420,22 +445,57 @@ struct tiny_size
 
 #endif
 
+//#define SLEEK_DESIGN
+#ifdef SLEEK_DESIGN
+
 template<>
 struct mpl::end_impl<tiny_tag>
 {
 	template<typename Tiny>
 	struct apply
 	{
-		typedef tiny_iterator < 
+		typedef tiny_iterator <
 			Tiny,
 			typename tiny_size<
-			typename Tiny::t0, 
-			typename Tiny::t1, 
+			typename Tiny::t0,
+			typename Tiny::t1,
 			typename Tiny::t2
 			>::type
-			>	type;
+		>	type;
 	};
 };
+
+#else
+
+// more manual (less elegant) end_impl:
+
+template<>
+struct mpl::end_impl<tiny_tag>
+{
+	// count how many none elements
+	template<typename Tiny>
+	struct apply {
+
+		// get constant wrapper:
+		typedef mpl::eval_if<
+			is_same<none, typename Tiny::t0>,	// if
+			int_<0>,							// then
+			mpl::eval_if<						// else
+			is_same<none, typename Tiny::t1>,	// if
+			int_<1>,							// then
+			mpl::eval_if<						// else
+			is_same<none, typename Tiny::t2>,	// if
+			int_<2>,							// then
+			int_<3>								// else
+			>
+			>
+		>	value_wrapper;
+		typedef tiny_iterator<Tiny, value_wrapper> type;
+
+	};
+};
+
+#endif
 
 #endif
 
@@ -495,7 +555,7 @@ struct mpl::push_front_impl<tiny_tag>
 template< typename Tiny, typename T, typename N>
 struct push_back_primitive
 {
-	static_assert( N::value < 3, "cannot push_back on a full tiny sequence");
+	static_assert(N::value < 3, "cannot push_back on a full tiny sequence");
 };
 
 
@@ -521,8 +581,8 @@ template<>
 struct mpl::push_back_impl<tiny_tag>
 {
 	template<typename Tiny, typename T>
-	struct apply 
-		: push_back_primitive<Tiny, T, typedef  typename tiny_size_composite<Tiny>::type>
+	struct apply
+		: push_back_primitive<Tiny, T, typename tiny_size_composite<Tiny>::type>
 	{};
 };
 
@@ -544,10 +604,56 @@ begin
 end
 push_back
 push_front
+tiny_size_composite
 */
 
 namespace TinySequenceImplementation
 {
+	template < template<typename...> typename List , typename... T >
+	constexpr typename std::enable_if< (sizeof...(T) <= 3), bool >::type
+		is_valid_tiny() { return true; }
+
+	template < template<typename...> typename List, typename... T >
+	constexpr typename std::enable_if< (sizeof...(T) > 3), bool >::type
+		is_valid_tiny() { return false; }
+
+	void testTiny()
+	{
+		// define four tiny sequences and test tiny_size_composite!
+		typedef tiny<> empty;
+		static_assert(tiny_size_composite<empty>::value == 0);
+		typedef tiny<int> one;
+		static_assert(tiny_size_composite<one>::value == 1);
+		typedef tiny<long, char*> two;
+		static_assert(tiny_size_composite<two>::value == 2);
+		typedef tiny<long, unsigned long, long[3]> three;
+		static_assert(tiny_size_composite<three>::value == 3);
+
+		// must have less than 3 parameters!!
+		static_assert(is_valid_tiny<tiny,int,int,int,int>() == false);
+		static_assert(is_valid_tiny<tiny, int, int, int>() == true);
+
+#if 0
+		template < typename... T >
+		constexpr typename std::enable_if< (sizeof...(T) <= 3), bool >::type
+			is_tiny(tiny<T...>) { return true; }
+
+		template < typename... T >
+		constexpr typename std::enable_if< (sizeof...(T) > 3), bool >::type
+			is_tiny(tiny<T...>) { return false; }
+
+		static_assert(is_valid_tiny<tiny<char, char, char, char>>() == false);
+		typedef tiny<char*, char, char, char> four;
+#endif
+		// test at:
+		typedef mpl::at<two, mpl::int_<1>>::type two_at_1;
+		static_assert(is_same<two_at_1, char*>::value);
+		// test begin and deref
+		typedef mpl::begin<three>::type begin_iter;
+		typedef mpl::deref<begin_iter>::type three_at_0;
+		static_assert(is_same<three_at_0, long>::value);
+		// .....
+	}
 	void useTiny()
 	{
 		typedef tiny<int, char*, long> t1;
@@ -563,10 +669,10 @@ namespace TinySequenceImplementation
 
 		typedef mpl::deref<iter>::type deref_t;
 		deref_t dt;
-		
+
 		typedef  mpl::begin<t1>::type  begin;
 
-		typedef  mpl::end<t1>::type  end;;
+		typedef  mpl::end<t1>::type  end;
 
 		typedef  mpl::advance<end, mpl::int_<-1>>::type last;
 		typedef  mpl::deref<last>::type last_value_type;
@@ -595,7 +701,7 @@ namespace TinySequenceImplementation
 		typedef  mpl::push_back<new_t3, unsigned long>::type new_t4;
 		new_t4 tt4;
 
-//#define WATCH_ERROR
+		//#define WATCH_ERROR
 #ifdef WATCH_ERROR
 		typedef typename mpl::push_back<new_t4, unsigned long>::type new_t5;
 		new_t5 tt5;
